ee4f3340cbe022ccdcaf9c6c2b4949fc54099b57
j.lee <j.lee@core-tech.jp>
2026-05-18 14:47:39 +0900

    status値毎にlabel追加

--- app/Enums/TaskStatus.php
+++ app/Enums/TaskStatus.php
@@ -7,4 +7,13 @@ enum TaskStatus: string
     case TODO = 'todo';
     case DOING = 'doing';
     case DONE = 'done';
+
+    public function label(): string
+    {
+        return match($this) {
+            self::TODO => '未着手',
+            self::DOING => '進行中',
+            self::DONE => '完了',
+        };
+    }
 }
