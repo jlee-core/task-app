03a8e863eef3af4cadce937c07b71914f2fc09b5
j.lee <j.lee@core-tech.jp>
2026-05-28 14:54:19 +0900

    意図的なciエラーを出すため

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -7,13 +7,12 @@ use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
+        // $response = $this->get('/');
 
-        $response->assertStatus(200);
+        // $response->assertStatus(200);
+
+        $this->fail('意図的な失敗：Branch Protection の動作確認用');
     }
 }
